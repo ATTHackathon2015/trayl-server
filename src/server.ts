d35c16476c6e4fc491c7c5b3a54db1857b901e16
Shuyang Chen <sc1085@gmail.com>
2015-09-12 17:07:23 -0400

    Added cors

--- src/server.ts
+++ src/server.ts
@@ -46,6 +46,12 @@ export default class Server {
 		
 		this.app.use(bodyParser.json());
 		
+		this.app.all('/', function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*");
+			res.header("Access-Control-Allow-Headers", "X-Requested-With");
+			next();
+		});
+		
 		this.addPost('travel');
 		this.addPost('add');
 		
