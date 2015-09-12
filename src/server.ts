238c03f2dc39bdbff8ddaa7c201785a015b47f4a
Shuyang Chen <sc1085@gmail.com>
2015-09-11 22:25:27 -0400

    Added nedb

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,25 @@
 import * as express from 'express';
 import {Express, Request, Response} from 'express';
 import * as bodyParser from 'body-parser';
+import Data from './data';
 
 export default class Server {
 	
 	app: Express;
+	
+	data: Data;
 	
 	constructor(public port: number) {
 		this.app = (<any>express).default();
 		
 		this.app.use(bodyParser.json());
 		
-		this.app.post('/index', this.index.bind(this));
+		this.app.post('/add', this.add.bind(this));
+		
+		this.data = new Data('data.db');
 	}
 	
-	index(req: Request, res: Response) {
+	add(req: Request, res: Response) {
 		res.json({
 			success: true,
 			data: req.body
@@ -22,8 +27,14 @@ export default class Server {
 	}
 	
 	listen() {
-		this.app.listen(this.port, () => {
-			console.log('[Server] Listening on port ' + this.port);
+		this.data.connect((error: Error) => {
+			if (error) {
+				console.error(error);
+				return;
+			}
+			this.app.listen(this.port, () => {
+				console.log('[Server] Listening on port ' + this.port);
+			});
 		});
 	}
 }
